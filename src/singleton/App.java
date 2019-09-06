b4bb6e9aa1559ac651f2e2bbb29da5b00cd208d6
Andres Cassanaz <andres.cassanaz@redb.ee>
2019-09-06 17:10:32 -0300

    Commit singleton

---
+++ src/singleton/App.java
@@ -0,0 +1,16 @@
+package singleton;
+
+import java.util.logging.Logger;
+
+public class App {
+
+    private final static Logger LOGGER = Logger.getLogger("App");
+
+
+    public static void main(String[] args) {
+        DbConector dbConectorOne = DbConector.getInstance();
+        DbConector dbConectorTwo = DbConector.getInstance();
+
+        LOGGER.info("Same Implementation: "+ dbConectorOne.equals(dbConectorOne));
+    }
+}
---
+++ src/singleton/DbConector.java
@@ -0,0 +1,35 @@
+package singleton;
+
+import java.util.logging.Logger;
+
+public class DbConector {
+
+    private final static Logger LOGGER = Logger.getLogger("DbConector");
+
+    private static DbConector instance;
+
+    private boolean isConnected = false;
+
+    private DbConector() {}
+
+    public static DbConector getInstance() {
+        if (instance == null) {
+            instance = new DbConector();
+        }
+        return instance;
+    }
+
+    public void connect(){
+        LOGGER.info("Connected to the database");
+        isConnected = true;
+    };
+
+    public void disconnect(){
+        LOGGER.info("Disconnected to the database");
+        isConnected = false;
+    };
+
+    public Boolean isConnected(){
+        return isConnected;
+    }
+}
